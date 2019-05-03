3a7afd2203f9a191f5c67114fd18604d8af6fd41
KEN <lumminary282@gmail.com>
2019-05-03 18:38:09 +0900

    create image

--- create_img.py
+++ create_img.py
@@ -4,8 +4,18 @@ import cv2
 img=np.zeros((400,400,3),np.uint8)
 
 img[:,:] =[255,0,0]
-cv2.imwrite('D:\GitHub\opencv4\OpenCV4\blueImage.jpg',img)
+
+cv2.imwrite('D:\\GitHub\\opencv4\\OpenCV4\\blueImage.jpg',img)
 cv2.imshow('img1',img)
+
+img[:,:] =[0,255,0]
+cv2.imwrite('D:\\GitHub\\opencv4\\OpenCV4\\greenImage.jpg',img)
+cv2.imshow('img2',img)
+
+img[:,:] =[0,0,255]
+cv2.imwrite('D:\\GitHub\\opencv4\\OpenCV4\\redImage.jpg',img)
+cv2.imshow('img3',img)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
